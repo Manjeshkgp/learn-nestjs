d6484930396de651c5deb1a6cd1b04441de3a0fc
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-12-22 23:21:16 +0530

    post method

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -20,5 +20,10 @@ export class CoffeesController {
     @Get("/flavours/:flavourId/:coffeeId")
     sendParams(@Param() params){ // here takes the whole params object
         return `This action returns #${params.coffeeId} coffee of #${params.flavourId} flavour`
+    }
+
+    @Post()
+    postBody(@Body() body){ // similar to @Params we can get only single key of the body object if required
+        return body
     }
 }
