08fc3fc9052d8794a17fbcb63a1989bc1a4e7ef5
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-12-23 15:28:51 +0530

    extra validation features

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // this will remove unecessary keys from body (or query / params) object via checking the DTO but not give 400 to api request
+    forbidNonWhitelisted: true // this will give 400 if something extra unknown is coming from body / params / query via checking DTO
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
