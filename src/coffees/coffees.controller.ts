ecdf251366b442a99c097560bb536f9d13603328
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-12-23 16:36:31 +0530

    Copied from chapter 1, started chapter 2

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    // const {limit,offset} = paginationQuery;
+    return this.coffeesService.findAll();
+  }
+
+  // @Get("/flavours")
+  // findFlavours() {
+  //     return "All flavours of coffee"
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    // it directly gets the id
+    console.log(typeof id)
+    return this.coffeesService.findOne('' + id);
+  }
+
+  // @Get("/flavours/:flavourId/:coffeeId")
+  // sendParams(@Param() params){ // here takes the whole params object
+  //     return `This action returns #${params.coffeeId} coffee of #${params.flavourId} flavour`
+  // }
+
+  @Post()
+  postBody(@Body() body: CreateCoffeeDto) {
+    // similar to @Params we can get only single key of the body object if required
+    console.log(body instanceof CreateCoffeeDto)
+    return this.coffeesService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // this will remove unecessary keys from body (or query / params) object via checking the DTO but not give 400 to api request
+    forbidNonWhitelisted: true, // this will give 400 if something extra unknown is coming from body / params / query via checking DTO
+    transform:true, // transforms the incoming data with the DTOs, so they will be the instanceof those DTOs
+  }));
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
